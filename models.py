4893ce85b9a8340301d6323f06bc6135bb4ef931
Corbin Simpson <MostAwesomeDude@gmail.com>
2010-11-25 21:22:13 -0800

    Add models.
    Converting to SQLAlchemy. Yay?

---
+++ models.py
@@ -0,0 +1,66 @@
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy import String, Integer, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Wordfilter(Base):
+    __tablename__ = "badwords"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    replacement = Column(String)
+
+class Category(Base):
+    __tablename__ = "boardcategory"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+
+class Board(Base):
+    __tablename__ = "board"
+
+    name = Column(String)
+    abbreviation = Column(String, primary_key=True)
+    category = Column(Integer, ForeignKey("boardcategory.id"))
+
+    def __init__(self, abbreviation, name):
+        self.abbreviation = abbreviation
+        self.name = name
+
+class Thread(Base):
+    __tablename__ = "thread"
+
+    id = Column(Integer, primary_key=True)
+    subject = Column(String)
+    author = Column(String)
+    board = Column(String, ForeignKey("board.abbreviation"))
+
+class Post(Base):
+    __tablename__ = "post"
+
+    id = Column(Integer, primary_key=True)
+    author = Column(String, nullable=False)
+    threadid = Column(Integer, ForeignKey("thread.id"))
+    timestamp = Column(DateTime)
+    comment = Column(String)
+    email = Column(String)
+
+class File(Base):
+    __tablename__ = "file"
+
+    id = Column(Integer, primary_key=True)
+    postid = Column(Integer, ForeignKey("post.id"))
+    filename = Column(String, nullable=False)
+
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine("sqlite:///test.db")
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)()
+
+    test = Board("test", "Test Board")
+    session.add(test)
+    session.commit()
